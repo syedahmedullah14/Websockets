5994c558db74431121753783e8c387dfe34d127c
Jaser <syedahmedullahjaser@gmail.com>
2025-03-26 15:33:23 +0530

    Setting client connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { WebSocketServer } from "ws"
 
+
 const app = express();
 const port =  8080;
 
@@ -12,7 +13,7 @@ const wss = new WebSocketServer({ server });
 
 wss.on("connection", (ws) => {
     ws.on("message", (data) => {
-        console.log("data from client :", data);
+        console.log("data from client %s:", data);
         ws.send("Thanks buddy!");
     })
 })
